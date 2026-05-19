2784d17b84c66ca482edfe22699e33279f2ae571
DeividRP <derodriguezp@unbosque.edu.co>
2026-05-18 22:34:57 -0500

    Arreglo Base de datos

--- DDeLi/ddeli/src/main/java/co/edu/unbosque/ddeli/entity/Usuario.java
+++ DDeLi/ddeli/src/main/java/co/edu/unbosque/ddeli/entity/Usuario.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -52,16 +53,16 @@ public class Usuario implements UserDetails {
 
 	private boolean enabled;
 
-	@OneToMany(mappedBy = "usuario")
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE, orphanRemoval = true)
 	private List<Pedido> pedidos;
 
-	@OneToMany(mappedBy = "usuario")
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE, orphanRemoval = true)
 	private List<Direccion> direcciones;
 
-	@OneToMany(mappedBy = "usuario")
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE, orphanRemoval = true)
 	private List<Evento> eventos;
 
-	@OneToMany(mappedBy = "usuario")
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE, orphanRemoval = true)
 	private List<Suscripcion> suscripciones;
 
 	
